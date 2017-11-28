d1cedf77978fb31ffb15f125b1b05d46c20b5fed
hejunqiu <xiaohe918@vip.qq.com>
2017-11-28 18:12:06 +0800

    Partial

---
+++ flag/Flag/Flag/flag.swift
@@ -0,0 +1,9 @@
+//
+//  flag.swift
+//  Flag
+//
+//  Created by hejunqiu on 2017/11/28.
+//  Copyright © 2017年 hejunqiu. All rights reserved.
+//
+
+import Foundation
---
+++ flag/Flag/Flag/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Flag
+//
+//  Created by hejunqiu on 2017/11/28.
+//  Copyright © 2017年 hejunqiu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
